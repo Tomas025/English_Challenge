0ba3be5ef46e474212b66d4e721ca497ce03d619
Tomas Silva <tomasbraz43@hotmail.com>
2022-06-23 22:32:57 -0300

    Feitoa a tela de jogo

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def homePage():
     return render_template('home/index.html')
+    
+@app.route('/jogo')
+def telajogo():
+    return render_template('ScreenGame/index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
